21ab614935b08aa9ac893f769c2f8b1d0f7e396b
Joseph A <joseph_anderson11@yahoo.com>
2022-09-15 15:56:32 -0400

    Link js file and create question object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 //Where to begin?
+//Array to hold questions
+var questionBank;
+
+//Create question object
+var question = {
+    questionText: "Is this working?",
+    option1: "Hello",
+    option2: "YES",
+    option3: "NO",
+    option4: "Hel",
+    correctAnswer: "Hello",
+    checkAnswer: function(a) {
+        if(a === this.correctAnswer){
+            console.log("Correct")
+        }else{
+            console.log("Incorrect")
+        }
+    }
+};
