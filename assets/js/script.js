58fbaaad458b726f62a3ade5c051863cf4ad6e30
Joseph A <joseph_anderson11@yahoo.com>
2022-09-16 10:02:39 -0400

    set up timer and timer start on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //Where to begin?
 //Array to hold questions
+var generateBtn = document.querySelector("#button1");
 var questionBank;
 
 //Create question object
@@ -18,3 +19,31 @@ var question = {
         }
     }
 };
+
+var questionBlock = document.getElementById("questionBlock");
+/*
+This is how we change the text in the <p> element of our staging area
+
+questionBlock.textContent = question.option1;
+*/
+
+var timer = document.getElementById("timer");
+
+/*
+This sets up the default timer. Need to replace "120" with a time variable
+*/
+timer.innerHTML = "Timer: " + "120";
+
+//Timer Function
+function countdown(){
+    var seconds = 119;
+    var clock = setInterval(function(){ timer.innerHTML = "Timer: " + seconds;
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(clock);
+        }
+    }, 1000);
+}
+
+//Starts timeer on button click
+generateBtn.addEventListener("click", countdown);
